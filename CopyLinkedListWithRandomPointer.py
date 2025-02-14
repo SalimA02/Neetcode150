b34e12e1b2ccdaa6958be2795486728474c12520
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2025-02-14 20:16:22 +0000

    Update CopyLinkedListWithRandomPointer.py

--- CopyLinkedListWithRandomPointer.py
+++ CopyLinkedListWithRandomPointer.py
@@ -26,3 +26,18 @@ class Solution:
                 copy = copy.next
 
         return new_head
+
+# HASHMAP ONE PASS
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        oldToCopy = collections.defaultdict(lambda: Node(0))
+        oldToCopy[None] = None
+        
+        cur = head
+        while cur:
+            oldToCopy[cur].val = cur.val
+            oldToCopy[cur].next = oldToCopy[cur.next]
+            oldToCopy[cur].random = oldToCopy[cur.random]
+            cur = cur.next
+        return oldToCopy[head]
