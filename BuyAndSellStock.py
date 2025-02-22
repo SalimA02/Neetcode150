f4b256939691e29bd07d36fb0b598701832bd7bd
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2025-02-22 21:26:06 +0000

    Update BuyAndSellStock.py

--- BuyAndSellStock.py
+++ BuyAndSellStock.py
@@ -9,12 +9,18 @@
 #         return res
 
 
+#2P
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
         maxP = 0
-        minBuy = prices[0]
 
-        for sell in prices:
-            maxP = max(maxP, sell - minBuy)
-            minBuy = min(minBuy, sell)
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                l = r
+            r += 1
         return maxP
