71c6a39e2258f33380d8eec33b2cb210addf2c51
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2025-03-01 20:28:52 +0000

    Update MergeKSortedLinkedLists.py

--- MergeKSortedLinkedLists.py
+++ MergeKSortedLinkedLists.py
@@ -1,26 +1,3 @@
-
-#ITERATION
-# class Solution:    
-#     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-#         res = ListNode(0)
-#         cur = res
-        
-#         while True:
-#             minNode = -1
-#             for i in range(len(lists)):
-#                 if not lists[i]:
-#                     continue
-#                 if minNode == -1 or lists[minNode].val > lists[i].val:
-#                     minNode = i
-            
-#             if minNode == -1:
-#                 break
-#             cur.next = lists[minNode]
-#             lists[minNode] = lists[minNode].next
-#             cur = cur.next
-
-#         return res.next
-
 # RECURSION
 
 # class Solution:
