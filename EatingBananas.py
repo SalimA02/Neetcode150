4ef1feba35c44b7a648f744f6eae08d066163b8d
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2025-03-20 05:07:50 +0000

    Update EatingBananas.py

--- EatingBananas.py
+++ EatingBananas.py
@@ -1,12 +1,17 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        speed = 1
-        while True:
+        l, r = 1, max(piles)
+        res = r
+
+        while l <= r:
+            k = (l + r) // 2
+
             totalTime = 0
-            for pile in piles:
-                totalTime += math.ceil(pile / speed)
-            
+            for p in piles:
+                totalTime += math.ceil(float(p) / k)
             if totalTime <= h:
-                return speed
-            speed += 1
-        return speed
+                res = k
+                r = k - 1
+            else:
+                l = k + 1
+        return res
