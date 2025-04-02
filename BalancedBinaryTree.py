5d5161c38578f2dcb86e49a8c2a2072e7983c925
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2025-04-02 09:59:30 +0100

    Update BalancedBinaryTree.py

--- BalancedBinaryTree.py
+++ BalancedBinaryTree.py
@@ -1,16 +1,30 @@
+# class Solution:
+#     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+#         if not root:
+#             return True
+        
+#         left = self.height(root.left)
+#         right = self.height(root.right)
+#         if abs(left - right) > 1:
+#             return False
+#         return self.isBalanced(root.left) and self.isBalanced(root.right)
+
+#     def height(self, root: Optional[TreeNode]) -> int:
+#         if not root:
+#             return 0
+
+#         return 1 + max(self.height(root.left), self.height(root.right))
+
+################### DFS ####################
+
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return True
-        
-        left = self.height(root.left)
-        right = self.height(root.right)
-        if abs(left - right) > 1:
-            return False
-        return self.isBalanced(root.left) and self.isBalanced(root.right)
+        def dfs(root):
+            if not root:
+                return [True, 0]
 
-    def height(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
+            left, right = dfs(root.left), dfs(root.right)
+            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+            return [balanced, 1 + max(left[1], right[1])]
 
-        return 1 + max(self.height(root.left), self.height(root.right))
+        return dfs(root)[0]
