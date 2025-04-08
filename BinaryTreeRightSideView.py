d1e592ac17ce015e2d17037cd2d8d285e5bd8056
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2025-04-08 09:21:08 +0100

    Update BinaryTreeRightSideView.py

--- BinaryTreeRightSideView.py
+++ BinaryTreeRightSideView.py
@@ -1,15 +1,20 @@
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         res = []
+        q = deque([root])
 
-        def dfs(node, depth):
-            if not node:
-                return None
-            if depth == len(res):
-                res.append(node.val)
-            
-            dfs(node.right, depth + 1)
-            dfs(node.left, depth + 1)
-        
-        dfs(root, 0)
+        while q:
+            rightSide = None
+            qLen = len(q)
+
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    rightSide = node
+                    q.append(node.left)
+                    q.append(node.right)
+            if rightSide:
+                res.append(rightSide.val)
         return res
+
+# O(n) Time and Space
