8bdd1410cd78b1f272d44386671f93a73467e2c8
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2025-04-12 20:49:03 +0100

    Update CountGoodNodesInBinaryTree.py

--- CountGoodNodesInBinaryTree.py
+++ CountGoodNodesInBinaryTree.py
@@ -17,3 +17,19 @@ class Solution:
                 q.append((node.right,max(maxval,node.val)))
                 
         return res
+
+
+# class Solution:
+#     def goodNodes(self, root: TreeNode) -> int:
+
+#         def dfs(node, maxVal):
+#             if not node:
+#                 return 0
+
+#             res = 1 if node.val >= maxVal else 0
+#             maxVal = max(maxVal, node.val)
+#             res += dfs(node.left, maxVal)
+#             res += dfs(node.right, maxVal)
+#             return res
+
+#         return dfs(root, root.val)
