7346aa0ffef389d22c7f9276ced645eaf91ebd9a
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2025-04-16 09:19:52 +0100

    Update ConstructBinaryTree.py

--- ConstructBinaryTree.py
+++ ConstructBinaryTree.py
@@ -1,20 +1,19 @@
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        indices = {val: idx for idx, val in enumerate(inorder)}
-        
-        self.pre_idx = 0
-        def dfs(l, r):
-            if l > r:
+        preIdx = inIdx = 0
+        def dfs(limit):
+            nonlocal preIdx, inIdx
+            if preIdx >= len(preorder):
                 return None
-
-            root_val = preorder[self.pre_idx]
-            self.pre_idx += 1
-            root = TreeNode(root_val)
-            mid = indices[root_val]
-            root.left = dfs(l, mid - 1)
-            root.right = dfs(mid + 1, r)
+            if inorder[inIdx] == limit:
+                inIdx += 1
+                return None
+            
+            root = TreeNode(preorder[preIdx])
+            preIdx += 1
+            root.left = dfs(root.val)
+            root.right = dfs(limit)
             return root
-
-        return dfs(0, len(inorder) - 1)
+        return dfs(float('inf'))
 
 # complete for AWS interview version
