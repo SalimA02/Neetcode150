3e2f44e22fad14525c350c17c77d90bccabf7dac
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2025-04-22 10:58:34 +0100

    Update LRUCache.py

--- LRUCache.py
+++ LRUCache.py
@@ -1,18 +1,27 @@
+# Without Built-In Function
 class LRUCache:
     def __init__(self, capacity: int):
-        self.cache = OrderedDict()
         self.cap = capacity
+        self.cache = {}
+        self.order = []
 
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
-        self.cache.move_to_end(key)
+        self.order.remove(key)
+        self.order.append(key)
         return self.cache[key]
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.cache.move_to_end(key)
-        self.cache[key] = value
-
-        if len(self.cache) > self.cap:
-            self.cache.popitem(last=False)
+            self.cache[key] = value
+            self.order.remove(key)
+            self.order.append(key)
+        else:
+            if len(self.cache) >= self.cap:
+                lru = self.order.pop(0)
+                del self.cache[lru]
+            self.cache[key] = value
+            self.order.append(key)
+            
+ # Less efficient than doubly linked list!
