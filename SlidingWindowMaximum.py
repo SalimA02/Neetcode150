55ca6d4f7fdba59111ce2c9d9f4ad04213b54537
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2025-04-30 11:20:28 +0100

    Update SlidingWindowMaximum.py

--- SlidingWindowMaximum.py
+++ SlidingWindowMaximum.py
@@ -18,3 +18,16 @@ class Solution:
             r += 1
 
         return output
+#  HEAP
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        heap = []
+        output = []
+        for i in range(len(nums)):
+            heapq.heappush(heap, (-nums[i], i))
+            if i >= k - 1:
+                while heap[0][1] <= i - k:
+                    heapq.heappop(heap)
+                output.append(-heap[0][0])
+        return output
