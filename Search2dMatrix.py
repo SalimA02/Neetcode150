61a625837b3016daf3e3c5a68c12d77a407130c2
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2025-06-12 16:56:59 +0100

    Update Search2dMatrix.py

--- Search2dMatrix.py
+++ Search2dMatrix.py
@@ -1,13 +1,16 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        m, n = len(matrix), len(matrix[0])
-        r, c = 0, n - 1
+        ROWS, COLS = len(matrix), len(matrix[0])
 
-        while r < m and c >= 0:
-            if matrix[r][c] > target:
-                c -= 1
-            elif matrix[r][c] < target:
-                r += 1
+        l, r = 0, ROWS * COLS - 1
+        while l <= r:
+            m = l + (r - l) // 2
+            row, col = m // COLS, m % COLS
+            if target > matrix[row][col]:
+                l = m + 1
+            elif target < matrix[row][col]:
+                r = m - 1
             else:
                 return True
         return False
+        
